d7e8bbc6b9b2d19000ba11c46bb345873a9d1705
Nigel Ferreres <nferrefe@gmail.com>
2024-11-12 23:29:30 +0100

    solved lab

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,64 @@
-function CountryDetails() {}
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default CountryDetails;
+
+
+function CountryDetailsPage() {
+  const [detailCountry, setDetailCountry] = useState();
+  const { countryId } = useParams();
+  const navigate = useNavigate();
+  const countryDetailUrl = `https://ih-countries-api.herokuapp.com/countries/${countryId}`;
+
+  async function getDetails() {
+    try {
+      const response = await fetch(countryDetailUrl);
+      const jsonResponse = await response.json();
+      setDetailCountry(jsonResponse);
+    } catch (error) {
+      console.log("This is the error: ", error);
+    }
+  }
+  
+
+  useEffect(() => {
+    getDetails();
+  }, [countryId]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate("/");
+  }
+
+  console.log("This is the array: ", detailCountry);
+
+  return (
+    <div>
+      <h2>Country Details</h2>
+      {detailCountry ? (
+        <article>
+          <img
+            src={`https://flagpedia.net/data/flags/icon/72x54/${detailCountry.alpha2Code.toLowerCase()}.png`}
+            alt={`Flag of ${detailCountry.name.common}`}
+          />
+          <h3>{detailCountry.name.common}</h3>
+          <p>Capital: {detailCountry.capital}</p>
+          <p>Area: {detailCountry.area} km²</p>
+          <p>Borders: </p>
+          <ul>
+            {detailCountry.borders.map((border, index) => (
+              <Link to={`/countries/${border}`}><li key={index}>{border}</li></Link>
+            ))}
+          </ul>
+        </article>
+      ) : (
+        <h3>Loading details...</h3>
+      )}
+      <button type="submit" onClick={handleSubmit}>
+        Back
+      </button>
+    </div>
+  );
+}
+
+export default CountryDetailsPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,48 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const apiUrl = "https://ih-countries-api.herokuapp.com/countries";
+
+function HomePage() {
+  const [countriesArray, setCountriesArray] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getCountries() {
+    try {
+      const response = await fetch(apiUrl);
+      const jsonResponse = await response.json();
+      setCountriesArray(jsonResponse);
+      setLoading(false);
+    } catch (error) {
+      console.log("This is the error: ", error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  return (
+    <div>
+      <h1>WikiCountries: Your Guide to the World</h1>
+      {loading ? (
+        <h3>Loading countries...</h3>
+      ) : (
+        countriesArray.map((country) => (
+          <article key={country._id}>
+            <Link to={`/countries/${country.alpha3Code}`}>
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt={`Flag of ${country.name.common}`}
+              />
+              <h3>{country.name.common}</h3>
+            </Link>
+          </article>
+        ))
+      )}
+    </div>
+  );
+}
 
 export default HomePage;
